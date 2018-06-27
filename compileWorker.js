c110338f7b76952a95df4e159b3c0a4ba5257fbc
Joshua Achorn <wight@cock.li>
2018-06-27 03:47:12 -0400

    .

--- compileWorker.js
+++ compileWorker.js
@@ -2,6 +2,12 @@ const fs = require("fs");
 const path = require('path');
 
 var fi = fs.readFileSync(path.join("lib", "IPC_worker_src.js")).toString();
-var built = "data:application/javascript," + encodeURIComponent(fi);
+var built = [
+  "module.exports = ",
+  '"',
+  "data:application/javascript,",
+  encodeURIComponent(fi),
+  '"'
+].join("");
 
 fs.writeFileSync(path.join("lib", "IPC_worker_built.js"), built);
